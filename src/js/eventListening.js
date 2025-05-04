5bcc40becea6f6d3b1b61570d434f0f34da2c054
凉白开喝上火 <1606813148@qq.com>
2025-05-04 22:26:00 +0800

    1.5

--- src/js/eventListening.js
+++
@@ -1,7 +0,0 @@
-$('.imgBreakButton').click(function () {
-    alert("回不去了！")
-})
-$(window).resize(function(){
-    $('.bodyMain-left').css({'height':window.innerHeight - 75 + 'px'})
-    $('.bodyMain-right').css({'height':window.innerHeight - 75 + 'px'})
-})
